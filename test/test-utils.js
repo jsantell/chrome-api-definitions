c10bb4f08b79856d041386a788ae0d55cf959886
Jordan Santell <jsantell@gmail.com>
2015-04-20 16:27:04 -0700

    Merge permission/context information into root and children function definitions

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,6 +1,7 @@
 var join = require("path").join;
 var expect = require("chai").expect;
 var utils = require("../lib/utils");
+var _ = require("underscore");
 var API_ROOT = join(__dirname, "..", "api");
 
 describe("utils", function () {
@@ -26,25 +27,55 @@ describe("utils", function () {
   describe(".getDefinition", function () {
     it("finds single namespace manifest in common (json)", function () {
       var def = utils.getDefinition("runtime", API_ROOT);
-      expect(def[0].namespace).to.be.equal("runtime");
+      expect(def.namespace).to.be.equal("runtime");
     });
     it("finds single namespace manifest in chrome (idl)", function () {
       var def = utils.getDefinition("browser", API_ROOT);
-      expect(def[0].namespace).to.be.equal("browser");
-      expect(def[0].functions.length).to.be.equal(1);
-      expect(def[0].types.length).to.be.equal(1);
+      expect(def.namespace).to.be.equal("browser");
+      expect(def.functions.length).to.be.equal(1);
+      expect(def.types.length).to.be.equal(1);
     });
     it("maps camel-cased name to underscored definition (browserAction -> browser_action)", function () {
       var def = utils.getDefinition("browserAction", API_ROOT);
-      expect(def[0].namespace).to.be.equal("browserAction");
-      expect(def[0].functions.length).to.be.equal(12);
-      expect(def[0].types.length).to.be.equal(2);
+      expect(def.namespace).to.be.equal("browserAction");
+      expect(def.functions.length).to.be.equal(12);
+      expect(def.types.length).to.be.equal(2);
     });
     it("maps camel-cased deep namespace (devtools.inspectedWindow -> devtools/inspected_window)", function () {
       var def = utils.getDefinition("devtools.inspectedWindow", API_ROOT);
-      expect(def[0].namespace).to.be.equal("devtools.inspectedWindow");
-      expect(def[0].functions.length).to.be.equal(3);
-      expect(def[0].types.length).to.be.equal(1);
+      expect(def.namespace).to.be.equal("devtools.inspectedWindow");
+      expect(def.functions.length).to.be.equal(3);
+      expect(def.types.length).to.be.equal(1);
+    });
+  });
+
+  describe(".attachMeta", function () {
+    it("does not attach meta when no permission or content_script defined", function () {
+      var def = utils.getDefinition("tabs", API_ROOT);
+      def = utils.attachMeta(def, "tabs", API_ROOT);
+      expect(def.content_script).to.be.equal(void 0);
+      expect(def.dependencies).to.be.equal(void 0);
+    });
+    it("attaches meta on the root when it has permission and content_script", function () {
+      var def = utils.getDefinition("storage", API_ROOT);
+      def = utils.attachMeta(def, "storage", API_ROOT);
+      expect(def.content_script).to.be.equal(true);
+      expect(def.dependencies).to.contain("permission:storage");
+    });
+    it("attaches meta on the root for deep namespaces", function () {
+      var def = utils.getDefinition("devtools.inspectedWindow", API_ROOT);
+      def = utils.attachMeta(def, "devtools.inspectedWindow", API_ROOT);
+      expect(def.content_script).to.be.equal(void 0);
+      expect(def.dependencies).to.contain("manifest:devtools_page");
+    });
+    it("attaches meta to children functions if found in manifest", function () {
+      var def = utils.getDefinition("runtime", API_ROOT);
+      def = utils.attachMeta(def, "runtime", API_ROOT);
+      var fn = _.findWhere(def.functions, { name: "sendMessage" });
+      expect(fn.content_script).to.be.equal(true);
+
+      fn = _.findWhere(def.functions, { name: "connectNative" });
+      expect(fn.dependencies).to.contain("permission:nativeMessaging");
     });
   });
 });
